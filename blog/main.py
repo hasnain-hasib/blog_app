3b33924b7959f343d5c9b7a3ee757cc996045a5a
Hasnain Hasib <hasibjoy332@gmail.com>
2024-07-14 22:58:15 +0600

    added user for Blog-App

--- blog/main.py
+++ blog/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Depends, HTTPException, status
 from typing import Optional
+from typing import List, Dict
 from . import schemas
 from .import models, schemas
 from .database import engine ,SessionLocal, get_db
@@ -11,23 +12,15 @@ models.Base.metadata.create_all(engine)
 
 
 @app.post('/blog', status_code= status.HTTP_201_CREATED)
-def create_blog(blog: schemas.Blog, db: Session = Depends(get_db)):
+def Create_Blog(blog: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=blog.title, body=blog.body)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return {"message": new_blog}
 
-
-# @app.delete("/blog/{id}",status_code= status.HTTP_204_NO_CONTENT)
-# def delete_blog(id , db: Session = Depends(get_db)):
-#     db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session= False)
-#     db.commit()
-#     return {"Blog" : "Deleted"}
-
-
 @app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def destroy(id: int, db: Session = Depends(get_db)):
+def Destroy(id: int, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
@@ -37,7 +30,7 @@ def destroy(id: int, db: Session = Depends(get_db)):
 
 
 @app.put('/blog/{id}',status_code=status.HTTP_202_ACCEPTED)
-def update_blog(id : int ,blog :schemas.Blog,db: Session = Depends(get_db)):
+def Update_Blog(id : int ,blog :schemas.Blog,db: Session = Depends(get_db)):
     blog_to_update = db.query(models.Blog).filter(models.Blog.id == id).first().update({"title" : "updated"})
     if blog_to_update:
         blog_to_update.title = blog.title
@@ -49,19 +42,27 @@ def update_blog(id : int ,blog :schemas.Blog,db: Session = Depends(get_db)):
 
 
 
-@app.get("/blog")
-def reading_blog(db : Session = Depends(get_db)):
+@app.get("/blog", response_model=List[schemas.ShowBlog])
+def All_BLog(db : Session = Depends(get_db)):
     blogs=db.query(models.Blog).all()
     return blogs
 
 
-@app.get('/blog/{id}')
-def reading_id(id , db :Session = Depends(get_db)):
+@app.get("/blog/{id}",response_model=schemas.ShowBlog)
+def Blog_ID(id , db :Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id ==id ).first()
     if not blog :
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"the id with {id} is not found")
-    #    return {"details of error" : f" the  id with {id} is not available"}
-        
+      
     return  blog
+
+@app.post("/user")
+def Create_User(user: schemas.User, db :Session = Depends(get_db)):
+    new_user= models.User(name =user.name ,email = user.email,password =user.password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+#title=blog.title, body=blog.body)
     
     
